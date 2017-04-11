9efe52be5e1d969f17932b2b6ca8f06bbfc04f1c
Mikhail Ziankevich <zin@zame-dev.org>
2017-04-11 22:39:09 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	entry: "./src/main.ts",
+
+	output: {
+		filename: "bundle.js",
+		path: __dirname + "/src",
+	},
+
+	resolve: {
+		extensions: ['.js', '.ts']
+	},
+
+	module: {
+		loaders: [
+			{
+				test: /\.ts?$/,
+				exclude: /node_modules/,
+				loaders: ["ts-loader"]
+			}
+		]
+	}
+};
