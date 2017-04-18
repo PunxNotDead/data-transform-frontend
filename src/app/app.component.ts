183c4676a467efc39caca997c4f53e1834f833a9
Mikhail Ziankevich <zin@zame-dev.org>
2017-04-18 21:01:26 +0200

    Drawing lines on click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,35 @@ import { Component } from "@angular/core";
 })
 
 class AppComponent {
+	x1: number;
+	x2: number;
+	y1: number;
+	y2: number;
+	mousePressed: boolean;
+
 	buttonClick(event) {
 		console.log(event);
+	}
+
+	startDraw(event: MouseEvent) {
+		this.x1 = event.clientX;
+		this.y1 = event.clientY;
+		this.x2 = event.clientX;
+		this.y2 = event.clientY;
+		this.mousePressed = true;
+	}
+
+	endDraw() {
+		this.mousePressed = false;
+	}
+
+	drawLine(event: MouseEvent) {
+		if (this.mousePressed) {
+			this.x2 = event.clientX;
+			this.y2 = event.clientY;
+		}
 	}
 }
 
 export default AppComponent;
+
